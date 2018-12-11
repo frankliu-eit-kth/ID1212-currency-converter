d2f0ca51c18722e3773a9df5d45d906e9797e6ce
frankliu-eit-kth <frankliu-eit-kth@users.noreply.github.com>
2018-12-11 14:29:59 +0100

    v1.1.1
    small change

--- src/main/java/model/ConvertRateRepo.java
+++ src/main/java/model/ConvertRateRepo.java
@@ -3,7 +3,12 @@ package model;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public interface ConvertRateRepo extends JpaRepository<ConvertRate, Integer> {
 	List<ConvertRate> findAll();
 	
--- src/main/java/model/CounterRepo.java
+++ src/main/java/model/CounterRepo.java
@@ -1,7 +1,12 @@
 package model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public interface CounterRepo extends JpaRepository<Counter, Integer> {
 	Counter findCounterById(int id);
 }
